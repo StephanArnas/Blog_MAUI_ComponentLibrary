841a6457d5400c0a1f9447317edff069cf9c65bb
Stephan Arnas <stephan.arnas@gmail.com>
2024-08-17 19:01:40 +0200

    fix: upd packages, add EntryHandler, remove Tizen

--- Blog_MAUI_Components/MauiProgram.cs
+++ Blog_MAUI_Components/MauiProgram.cs
@@ -22,20 +22,38 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, _) =>
-        {
-#if __ANDROID__
-            // Remove the underline from the EditText.
-            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
-#endif
-        });
-
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
+        ApplyStyleCustomization();
+        
+        // Register your pages.
         Routing.RegisterRoute(RouteConstants.EntryPage, typeof(EntryPage));
 
         return builder.Build();
+    }
+    
+    private static void ApplyStyleCustomization()
+    {
+        EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, _) =>
+        {
+#if __ANDROID__
+            // Remove the underline from the EditText
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#endif
+        });
+        
+        EntryHandler.Mapper.AppendToMapping("SetUpEntry", (handler, view) =>
+        {
+#if ANDROID
+
+#elif IOS || MACCATALYST
+            // Remove outline from the UITextField
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+      
+#endif
+        });
     }
 }
