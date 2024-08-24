7eb2c07a46dc207bf87306f3176a066c83fe6de4
Stephan Arnas <stephan.arnas@gmail.com>
2024-08-24 16:20:53 +0200

    feat: part_2 use static property for the fluent validation instead of string

--- Blog_MAUI_Components/Presentation/Pages/Entry/EntryPageViewModelValidator.cs
+++ Blog_MAUI_Components/Presentation/Pages/Entry/EntryPageViewModelValidator.cs
@@ -4,13 +4,18 @@ namespace Blog_MAUI_Components.Presentation.Pages.Entry;
 
 public class EntryPageViewModelValidator : AbstractValidator<EntryPageViewModel>
 {
+    public static string FullNameProperty => nameof(EntryPageViewModel.FullName);
+    public static string EmailProperty => nameof(EntryPageViewModel.Email);
+
     public EntryPageViewModelValidator()
     {
         RuleFor(x => x.FullName)
-            .NotNull();
+            .NotNull()
+            .NotEmpty();
         
         RuleFor(x => x.Email)
             .NotNull()
+            .NotEmpty()
             .EmailAddress();
     }
 }
